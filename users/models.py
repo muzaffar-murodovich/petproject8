2764dfe305d3fca38662d21fee2a29ac23f6be98
Muzaffar Murodovich <muzaffarmurodogli@gmail.com>
2025-11-06 19:13:08 +0500

    +um

--- users/models.py
+++ users/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 from shared.models import BaseModel
 from datetime import datetime, timedelta
 from django.core.validators import FileExtensionValidator
+import uuid
+from rest_framework_simplejwt.tokens import RefreshToken
 
 ORDINARY_USER, MANAGER, ADMIN = ('ordinary_user', 'manager', 'admin')
 VIA_EMAIL, VIA_PHONE = ("via_email", "via_phone")
@@ -50,6 +52,46 @@ class User(AbstractUser, BaseModel):
             code=str(code)
         )
         return code
+    
+    def check_username(self):
+        if not self.username:
+            temp_username = f"instagram-{uuid.uuid4().__str__().split('-')[-1]}"
+            while User.objects.filter(username=temp_username).exists():
+                temp_username = f"instagram-{uuid.uuid4().__str__().split('-')[-1]}"
+            self.username = temp_username
+
+    def check_email(self):
+        if self.email:
+            normalize_email = self.email.lower()
+            self.email = normalize_email
+    
+    def check_pass(self):
+        if not self.password:
+            temp_password = f"password-{uuid.uuid4().__str__().split('-')[-1]}"
+            self.password = temp_password
+    
+    def hashing_password(self):
+        if not self.password.startswith('pbkdf2_sha256$'):
+            self.set_password(self.password)
+    
+    def token(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+        }
+
+    def clean(self):
+        self.check_username()
+        self.check_email()
+        self.check_pass()
+        self.hashing_password()
+        # super(User, self).clean()
+    
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.clean()
+        super(User, self).save(*args, **kwargs)
 
 PHONE_EXPIRE = 2
 EMAIL_EXPIRE = 5

94cc95b68c33cb36111a4747beecd094c0171e99
Muzaffar Murodovich <muzaffarmurodogli@gmail.com>
2025-11-11 18:54:55 +0500

    +post

--- post/models.py
+++ post/models.py
@@ -1,8 +1,61 @@
+from django.core.validators import FileExtensionValidator, MaxLengthValidator
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models import UniqueConstraint
+
+from shared.models import BaseModel
 # Create your models here.
 
 User = get_user_model()
 
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    image = models.ImageField(upload_to='post_images/', validators=[
+        FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
+    caption = models.TextField(validators=[MaxLengthValidator(200)])
+
+    class Meta:
+        db_table = 'posts'
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+
+class PostComment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    comment = models.TextField(validators=[MaxLengthValidator(200)])
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='children',
+        null=True,
+        blank=True
+    )
+    # class Meta:
+    #     db_table = 'comments'
+    #     verbose_name = 'comment'
+    #     verbose_name_plural = 'comments'
+
+class PostLike(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['author', 'post'],
+            )
+        ]
+        db_table = 'post_likes'
+        verbose_name = 'like'
+        verbose_name_plural = 'likes'
+
+class CommentLike(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(PostComment, on_delete=models.CASCADE, related_name='likes')
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['author', 'comment'],
+            )
+        ]

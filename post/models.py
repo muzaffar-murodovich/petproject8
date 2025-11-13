cb7db965f8283bc435103ccb3c9fbddb3011de73
Muzaffar Murodovich <muzaffarmurodogli@gmail.com>
2025-11-13 18:39:50 +0500

    admin panel: post

--- post/models.py
+++ post/models.py
@@ -1,53 +1,59 @@
+from django.contrib.auth import get_user_model
 from django.core.validators import FileExtensionValidator, MaxLengthValidator
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.db.models import UniqueConstraint
 
 from shared.models import BaseModel
-# Create your models here.
 
 User = get_user_model()
 
-class Post(models.Model):
+
+class Post(BaseModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(upload_to='post_images/', validators=[
-        FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
-    caption = models.TextField(validators=[MaxLengthValidator(200)])
+    image = models.ImageField(upload_to='post_images', validators=[
+        FileExtensionValidator(allowed_extensions=['jpeg', 'jpg', 'png'])])
+    caption = models.TextField(validators=[MaxLengthValidator(2000)])
 
     class Meta:
-        db_table = 'posts'
-        verbose_name = 'post'
-        verbose_name_plural = 'posts'
+        db_table = "posts"
+        verbose_name = "post"
+        verbose_name_plural = "posts"
 
-class PostComment(models.Model):
+    def __str__(self):
+        return f"{self.author} post about {self.caption}"
+
+
+class PostComment(BaseModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    comment = models.TextField(validators=[MaxLengthValidator(200)])
+    comment = models.TextField()
     parent = models.ForeignKey(
         'self',
         on_delete=models.CASCADE,
-        related_name='children',
+        related_name='child',
         null=True,
         blank=True
     )
-    # class Meta:
-    #     db_table = 'comments'
-    #     verbose_name = 'comment'
-    #     verbose_name_plural = 'comments'
 
-class PostLike(models.Model):
+    def __str__(self):
+        return f"comment by {self.author}"
+
+
+class PostLike(BaseModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
 
     class Meta:
         constraints = [
-            UniqueConstraint(
-                fields=['author', 'post'],
-            )
+          UniqueConstraint(
+              fields=['author', 'post'],
+              name='postLikeUnique'
+          )
         ]
-        db_table = 'post_likes'
-        verbose_name = 'like'
-        verbose_name_plural = 'likes'
+
+    def __str__(self):
+        return f"{self.author} post like {self.post}"
+
 
 class CommentLike(BaseModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -55,7 +61,11 @@ class CommentLike(BaseModel):
 
     class Meta:
         constraints = [
-            UniqueConstraint(
-                fields=['author', 'comment'],
-            )
+          UniqueConstraint(
+              fields=['author', 'comment'],
+              name='CommentLikeUnique'
+          )
         ]
+
+    def __str__(self):
+        return f"{self.author} comment like {self.comment}"
